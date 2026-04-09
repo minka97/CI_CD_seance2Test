2d0e522570b1ad0dabc0c293d48be12078b6b647
minka <minkaelcisse97@gmail.com>
2026-04-09 11:22:21 +0200

    test(coverage): configurer les seuils de couverture a 80%

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @format */
 
-import { createDefaultPreset } from "ts-jest";
-
 /** @type {import("jest").Config} */
 const config = {
   preset: "ts-jest/presets/default-esm",
@@ -14,6 +12,21 @@ const config = {
 
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/tests/**",
+    "!src/types/**",
+  ],
+
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
   },
 };
 
